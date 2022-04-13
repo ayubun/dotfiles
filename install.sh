225f9cdc8b5c18f9f7342600b7bfa262e94c360f
Ayu <ayu@ayu.dev>
2022-04-12 22:29:33 -0700

    Add docker & docker-compose installer

--- install.sh
+++ install.sh
@@ -1,8 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
 DOTFILES_FOLDER=$HOME/dotfiles
 
 find $DOTFILES_FOLDER/config -maxdepth 1 -mindepth 1 -type f -printf "%f\n" | \
 while read file; do
+    rm -rf $HOME/$file
     ln -s $DOTFILES_FOLDER/config/$file $HOME/$file
+done
+
+find $DOTFILES_FOLDER/programs/ubuntu -maxdepth 1 -mindepth 1 -type f -printf "%f\n" | \
+while read file; do
+    source $DOTFILES_FOLDER/programs/ubuntu/$file
 done
