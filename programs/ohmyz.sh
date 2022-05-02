bacb1138370435d03a2c1b3e9690bd0b301ac458
Ayu <ayu@ayu.dev>
2022-05-02 12:49:50 -0700

    Fix: Ohmyzsh exiting on installer

--- programs/ohmyz.sh
+++ programs/ohmyz.sh
@@ -18,6 +18,7 @@ fi
 # Remove any old installation, if present
 rm -r $HOME/.oh-my-zsh
 # Install Oh My Zsh
+export RUNZSH=no
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 # Install Powerlevel10k theme (https://github.com/romkatv/powerlevel10k)
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
