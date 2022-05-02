be492f2f5d30dd4da68e4df6c50be21122d56ab1
Ayu <ayu@ayu.dev>
2022-05-02 12:25:09 -0700

    Fix: Multiline cat bug

--- programs/ohmyz.sh
+++ programs/ohmyz.sh
@@ -32,7 +32,7 @@ fi
 
 cat <<EOF >> ~/.zshrc
 
-# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+# To customize prompt, run p10k configure or edit ~/.p10k.zsh.
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
 
 # Run neofetch on terminal login! (just looks kinda cool :3)
