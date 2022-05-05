1f57a7425c52ac0da9fc2e123740b37b89f6bcef
Ayu <ayu@ayu.dev>
2022-05-05 19:49:09 +0000

    Remove workspace hostname modifier

--- programs/ubuntu/change-workspace-hostname.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# For some reason.. coder renames itself to workspace.
-# I want my terminal to show milk-tea though because thats
-# much cuter.. lol
-if grep -q workspace "/etc/hostname"; then
-  sudo hostnamectl set-hostname "milk-tea" &>/dev/null
-  sudo hostnamectl set-hostname "milk-tea" --pretty&>/dev/null
-fi
