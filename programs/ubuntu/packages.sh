29b64d7b3f9c8b8d3110e02a4e73eb2a4215b998
Ayu <ayu@ayu.dev>
2022-12-14 00:41:10 -0800

    Add python3.8 & gcloud cli installs

--- programs/ubuntu/packages.sh
+++ programs/ubuntu/packages.sh
@@ -12,4 +12,18 @@ sudo apt-get install neofetch -y
 sudo apt-get install google-cloud-sdk-pubsub-emulator -y
 sudo apt-get install net-tools -y
 
+# python 3.8
+sudo apt install software-properties-common -y
+sudo add-apt-repository ppa:deadsnakes/ppa -y 
+sudo apt update -y 
+sudo apt install python3.8
+
+# gcloud cli
+sudo apt-get install apt-transport-https ca-certificates gnupg -y &> /dev/null
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list &> /dev/null
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add - &> /dev/null
+sudo apt-get update -y &> /dev/null
+sudo apt-get install google-cloud-cli -y &> /dev/null
+sudo apt-get install google-cloud-cli-cbt -y &> /dev/null
+
 sudo apt autoremove -y
