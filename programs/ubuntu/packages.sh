945e11150881827f6de1a111221fb1cf45ce348d
ayu <ayu@ayu.dev>
2023-05-01 17:07:20 -0700

    remove python 3.8 install

--- programs/ubuntu/packages.sh
+++ programs/ubuntu/packages.sh
@@ -13,13 +13,13 @@ sudo apt-get install google-cloud-sdk-pubsub-emulator -y
 sudo apt-get install net-tools -y
 
 # python 3.8
-sudo apt install software-properties-common -y
-sudo add-apt-repository ppa:deadsnakes/ppa -y 
-sudo apt update -y 
-sudo apt install python3.8
+# sudo apt install software-properties-common -y
+# sudo add-apt-repository ppa:deadsnakes/ppa -y 
+# sudo apt update -y 
+# sudo apt install python3.8
 
 # cleanup
-sudo apt update -y
-sudo apt upgrade -y
-sudo apt --fix-broken install -y 
-sudo apt autoremove -y
+# sudo apt update -y
+# sudo apt upgrade -y
+# sudo apt --fix-broken install -y 
+# sudo apt autoremove -y
