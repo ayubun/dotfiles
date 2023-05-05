d73d7d40a89733676510224121ecbc557f19fcff
ayu <ayu@ayu.dev>
2023-05-05 03:44:08 -0700

    fix perms issue

--- programs/ubuntu/packages.sh
+++ programs/ubuntu/packages.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+fix-apt() {
+    ALREADY_FIXING=false
+    while ! { set -C; 2>/dev/null >$HOME/dotfiles/tmp/apt-fix.lock; }; do
+        ALREADY_FIXING=true
+        sleep 1
+    done
+    if [[ $ALREADY_FIXING == true ]]; then
+        rm -f $HOME/dotfiles/tmp/apt-fix.lock
+        return 0
+    fi
+    sudo apt-fast --fix-broken install -y &>/dev/null
+    sudo apt-fast --fix-missing install -y &>/dev/null
+    sudo apt-fast install -f -y &>/dev/null
+    rm -f $HOME/dotfiles/tmp/apt-fix.lock
+}
+export -f fix-apt
+
 packages=(
     'build-essential'
     'manpages-dev'
@@ -23,4 +40,27 @@ for repository in ${apt_repositories[@]}; do
 done
 sudo apt-fast update -y
 sudo apt-fast upgrade -y
-sudo apt-fast -y install "${packages[@]}"
+
+install_package() {
+    if ! sudo apt-fast -y install $1; then
+        fix-apt
+        sudo apt-fast -y install $1
+    fi
+}
+export -f install_package
+
+install_all_packages() {
+    ATTEMPTS=0
+    while ! sudo apt-fast -y install "${packages[@]}"; do
+        ATTEMPTS=$ATTEMPTS+1
+        if [[ $ATTEMPTS > 4 ]]; then
+            echo "Max apt install attempts reached"
+            exit 1
+        fi
+        fix-apt
+    done
+}
+export -f install_all_packages
+
+install_all_packages
+sudo apt-fast autoremove -y
