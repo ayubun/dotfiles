ab3f9cd3a59f9813977b52a8ae0b6bed8156f2fa
ayu <ayu@ayu.dev>
2025-03-20 15:24:24 -0700

    configure goto preview lvim

--- configs/lvim/config.lua
+++ configs/lvim/config.lua
@@ -60,27 +60,6 @@ lvim.plugins = {
   },
   { "EdenEast/nightfox.nvim" },
   { "easymotion/vim-easymotion" },
-  -- { "Shatur/neovim-ayu" },
-  -- {
-  --   "simrat39/rust-tools.nvim",
-  --   ft = { "rust" },
-  --   config = function()
-  --     require("rust-tools").setup {
-  --       tools = {
-  --         autoSetHints = true,
-  --         hover_with_actions = true,
-  --         runnables = {
-  --           use_telescope = true,
-  --         },
-  --         inlay_hints = {
-  --           show_parameter_hints = true,
-  --            parameter_hints_prefix = " <- ",
-  --           other_hints_prefix = " => ",
-  --         },
-  --       },
-  --     }
-  --   end,
-  -- },
 }
 
 local ok, copilot = pcall(require, "copilot")
@@ -101,4 +80,30 @@ copilot.setup {
 
 local opts = { noremap = true, silent = true }
 vim.api.nvim_set_keymap("n", "<c-s>", "<cmd>lua require('copilot.suggestion').toggle_auto_trigger()<CR>", opts)
+
+require('goto-preview').setup {
+  width = 120, -- Width of the floating window
+  height = 15, -- Height of the floating window
+  border = {"↖", "─" ,"┐", "│", "┘", "─", "└", "│"}, -- Border characters of the floating window
+  default_mappings = true, -- Bind default mappings
+  debug = false, -- Print debug information
+  opacity = nil, -- 0-100 opacity level of the floating window where 100 is fully transparent.
+  resizing_mappings = false, -- Binds arrow keys to resizing the floating window.
+  post_open_hook = nil, -- A function taking two arguments, a buffer and a window to be ran as a hook.
+  post_close_hook = nil, -- A function taking two arguments, a buffer and a window to be ran as a hook.
+  references = { -- Configure the telescope UI for slowing the references cycling window.
+    provider = "telescope", -- telescope|fzf_lua|snacks|mini_pick|default
+    telescope = require("telescope.themes").get_dropdown({ hide_preview = false })
+  },
+  -- These two configs can also be passed down to the goto-preview definition and implementation calls for one off "peak" functionality.
+  focus_on_open = true, -- Focus the floating window when opening it.
+  dismiss_on_move = false, -- Dismiss the floating window when moving the cursor.
+  force_close = true, -- passed into vim.api.nvim_win_close's second argument. See :h nvim_win_close
+  bufhidden = "wipe", -- the bufhidden option to set on the floating window. See :h bufhidden
+  stack_floating_preview_windows = true, -- Whether to nest floating windows
+  same_file_float_preview = true, -- Whether to open a new floating window for a reference within the current file
+  preview_window_title = { enable = true, position = "left" }, -- Whether to set the preview window title as the filename
+  zindex = 1, -- Starting zindex for the stack of floating windows
+  vim_ui_input = true, -- Whether to override vim.ui.input with a goto-preview floating window
+}
 
