9d9137a85c3178ea8d1fb33839247db09acb0c75
ayu <ayu@ayu.dev>
2025-03-19 17:35:03 -0700

    try diff setup lol

--- configs/lvim/config.lua
+++ configs/lvim/config.lua
@@ -20,22 +20,41 @@ lvim.builtin.which_key.mappings["t"] = {
   h = { "<cmd>2ToggleTerm size=30 direction=horizontal<cr>", "Split horizontal" },
 }
 
--- Configure clipboard provider
+-- -- Configure clipboard provider
+-- vim.g.clipboard = {
+--   name = 'OSC 52',
+--   copy = {
+--     ['+'] = {
+--       'bash', '-c',
+--       'base64 | xargs printf "\\033]52;c;%s\\033\\"'
+--     },
+--     ['*'] = {
+--       'bash', '-c',
+--       'base64 | xargs printf "\\033]52;c;%s\\033\\"'
+--     },
+--   },
+--   paste = {
+--     ['+'] = {'bash', '-c', 'ghostty clipboard read'},
+--     ['*'] = {'bash', '-c', 'ghostty clipboard read'},
+--   },
+--   cache_enabled = 0,
+-- }
 vim.g.clipboard = {
-  name = 'OSC 52',
+  name = 'OSC 52 (Universal)',
   copy = {
     ['+'] = {
-      'bash', '-c',
-      'base64 | xargs printf "\\033]52;c;%s\\033\\"'
+      'sh', '-c',
+      -- This sends the OSC 52 sequence directly to the terminal
+      "printf '\\033]52;c;%s\\033\\\\' \"$(cat | base64)\""
     },
     ['*'] = {
-      'bash', '-c',
-      'base64 | xargs printf "\\033]52;c;%s\\033\\"'
+      'sh', '-c',
+      "printf '\\033]52;c;%s\\033\\\\' \"$(cat | base64)\""
     },
   },
   paste = {
-    ['+'] = {'bash', '-c', 'ghostty clipboard read'},
-    ['*'] = {'bash', '-c', 'ghostty clipboard read'},
+    ['+'] = {'sh', '-c', 'echo "OSC 52 paste not supported"'},
+    ['*'] = {'sh', '-c', 'echo "OSC 52 paste not supported"'},
   },
   cache_enabled = 0,
 }
