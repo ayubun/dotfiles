9d3c59f55e9e83c7e116b548540ab0278a1d89ad
ayu <ayu@ayu.dev>
2025-03-27 11:55:52 -0700

    aaa

--- configs/nvim/plugins/example.lua
+++ configs/nvim/plugins/example.lua
@@ -125,7 +125,7 @@ return {
 	},
 
 	-- use mini.starter instead of alpha
-	{ import = "lazyvim.plugins.extras.ui.mini-starter" },
+	-- { import = "lazyvim.plugins.extras.ui.mini-starter" },
 
 	-- add jsonls and schemastore packages, and setup treesitter for json, json5 and jsonc
 	{ import = "lazyvim.plugins.extras.lang.json" },
