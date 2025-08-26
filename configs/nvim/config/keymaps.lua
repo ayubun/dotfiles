5005fa187d0f0fdd33171576281205feca087ea2
ayu <ayu@ayu.dev>
2025-08-26 23:50:24 +0000

    more keybinding changes

--- configs/nvim/config/keymaps.lua
+++ configs/nvim/config/keymaps.lua
@@ -1,13 +1,13 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-local M = {}
-
-M.general = {
-  n = {
-    ["<C-h>"] = { "<cmd> TmuxNavigateLeft<CR>", "window left" },
-    ["<C-l>"] = { "<cmd> TmuxNavigateRight<CR>", "window right" },
-    ["<C-j>"] = { "<cmd> TmuxNavigateDown<CR>", "window down" },
-    ["<C-k>"] = { "<cmd> TmuxNavigateUp<CR>", "window up" },
-  }
-}
+-- local M = {}
+--
+-- M.general = {
+--   n = {
+--     ["<C-h>"] = { "<cmd> TmuxNavigateLeft<CR>", "window left" },
+--     ["<C-l>"] = { "<cmd> TmuxNavigateRight<CR>", "window right" },
+--     ["<C-j>"] = { "<cmd> TmuxNavigateDown<CR>", "window down" },
+--     ["<C-k>"] = { "<cmd> TmuxNavigateUp<CR>", "window up" },
+--   }
+-- }
--- configs/nvim/plugins/main.lua
+++ configs/nvim/plugins/main.lua
@@ -12,6 +12,21 @@ return {
   {
     "christoomey/vim-tmux-navigator",
     lazy = false,
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
+      "TmuxNavigatorProcessList",
+    },
+    keys = {
+      { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+      { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+      { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+      { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+    },
   },
   -- https://github.com/ibhagwan/smartyank.nvim
   -- yank thru ssh / tmux / etc
