22762abdd2131c2191472b9090de59e1b38eeda2
ayu <ayu@ayu.dev>
2025-09-16 14:55:12 -0700

    tpm install non-root

--- programs/tmux-tpm.sh
+++ programs/tmux-tpm.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
 if [ -d ~/.tmux/plugins/tpm ]; then
-    echo "tmux tpm is already installed~"
-else
-    # freshly install tpm
-    rm -rf ~/.tmux/plugins/tpm || true
-    mkdir -p ~/.tmux/plugins/tpm || true
-    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-    echo ""
-    echo "tmux tpm is now installed~"
+  echo "tmux tpm is already installed~"
+  exit 0
 fi
+
+sudo rm -rf ~/.tmux/plugins/tpm || true
+
+if [[ -n "$ORIGINAL_USER" && "$ORIGINAL_USER" != "root" ]]; then
+  # Run as original user using sudo -u
+  sudo -u "$ORIGINAL_USER" -H bash -c "mkdir -p ~/.tmux/plugins/tpm || true"
+  sudo -u "$ORIGINAL_USER" -H bash -c "git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm"
+else
+  echo "⚠️WARNING: the dotfiles were run as a root user, meaning tpm cannot be installed as non-root. Installing as root..." 
+  mkdir -p ~/.tmux/plugins/tpm || true
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+fi
+
+echo ""
+echo "tmux tpm is now installed~"
+
