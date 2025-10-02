208d28ae58851eb124359a3d80e78390a7560469
ayu <ayu@ayu.dev>
2025-10-02 15:02:23 -0700

    unattended ohmyzsh install

--- programs/ohmyz.sh
+++ programs/ohmyz.sh
@@ -38,7 +38,8 @@ sudo rm -r $HOME/.oh-my-zsh
 # Install Oh My Zsh
 export RUNZSH=no
 export KEEP_ZSHRC=yes
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --keep-zshrc
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --keep-zshrc --unattended
+chsh -s $(which zsh) &>/dev/null
 # Install Powerlevel10k theme (https://github.com/romkatv/powerlevel10k)
 mkdir -p ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
