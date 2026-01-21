f51b64f274287679a99376916395a2136baa4063
ayu <ayu@ayu.dev>
2026-01-21 14:58:26 -0800

    bugfixes with pipx/nvr?

--- programs/pipx.sh
+++ programs/pipx.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+if [[ -n "$ORIGINAL_USER" && "$ORIGINAL_USER" != "root" ]]; then
+  # downgrade to original user
+  su - $ORIGINAL_USER
+else
+  echo "⚠️WARNING: the dotfiles were run as a root user, meaning pipx cannot install programs as non-root. Installing as root..." 
+fi
+
+# ensure that the user and group of pipx is actually of the original user .. (otherwise this causes issues..)
+sudo chown -R $(id -u):$(id -g) ~/.local/pipx &>/dev/null
+
 # https://github.com/mhinz/neovim-remote
 pipx install neovim-remote
 pipx install pynvim --upgrade
