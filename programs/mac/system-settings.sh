f674ee0bada859404bf223c71d7cf483dcc10198
ayu <ayu@ayu.dev>
2026-04-02 16:13:56 -0700

    fix screenshots dir perms

--- programs/mac/system-settings.sh
+++ programs/mac/system-settings.sh
@@ -279,6 +279,7 @@ defaults write com.apple.screensaver askForPasswordDelay -int 0
 
 # Don't save screenshots to the Desktop
 mkdir -p "${HOME}/Screenshots"
+chown "${SUDO_USER:-$(whoami)}" "${HOME}/Screenshots"
 defaults write com.apple.screencapture location -string "${HOME}/Screenshots"
 
 # Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF)
