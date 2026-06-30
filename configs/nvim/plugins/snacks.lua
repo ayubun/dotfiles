cf9121d50e7abc9c91dfd5d7e772a8e51fea1cfb
ayu <ayu@ayu.dev>
2026-06-30 13:21:39 -0700

    show snacks hidden files by default

--- configs/nvim/plugins/snacks.lua
+++ configs/nvim/plugins/snacks.lua
@@ -11,6 +11,8 @@ return {
         },
       },
       sources = {
+        files = { hidden = true },
+        grep = { hidden = true },
         explorer = {
           diagnostics = false,
           diagnostics_open = false,
